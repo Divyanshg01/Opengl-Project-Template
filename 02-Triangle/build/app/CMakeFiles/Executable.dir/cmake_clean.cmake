8989d77234b42dd0256c019c8c81f64b04d48a1d
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2024-07-24 19:21:47 +0530

    VAO

---
+++ 02-Triangle/build/app/CMakeFiles/Executable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Executable.dir/main.cpp.o"
+  "CMakeFiles/Executable.dir/main.cpp.o.d"
+  "Executable"
+  "Executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
